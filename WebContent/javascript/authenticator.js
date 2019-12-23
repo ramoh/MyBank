aa7ab484b1c53b8388e01fc19b335f0e24ac27f3
ramoh <mohantyrajesh1984@gmail.com>
2019-12-23 18:00:07 +0530

    authentication 2

--- WebContent/javascript/authenticator.js
+++ WebContent/javascript/authenticator.js
@@ -12,10 +12,13 @@ var Authenticator = function (serverModule) {
     //callback for successful login
     var loginCallback;
 
+    //flag to show authentication has failed
+    var showAuthenticationFailed = ko.observable(false);
+
     //user credentials
     var credential = {
-        userName: ko.observable(),
-        password: ko.observable()
+        userName: ko.observable().extend({required: true}),
+        password: ko.observable().extend({required: true})
     };
 
     //authentications token
@@ -29,7 +32,18 @@ var Authenticator = function (serverModule) {
     });
 
     var login = function () {
+        //check for validations
+        if (credential.errors().length > 0) {
+            console.log("Credential model in invalid");
+            credential.errors.showAllMessages();
+            return;
+        }
         var token = server.login(credential.userName(), credential.password());
+        if (token == false) {
+            showAuthenticationFailed(true);
+            return;
+
+        }
         authenticationToken(token);
         console.log("login :" + authenticationToken());
         loginCallback();
@@ -39,8 +53,23 @@ var Authenticator = function (serverModule) {
     var setCallBack = function (callBack) {
         loginCallback = callBack;
     };
+
+    var loggedInUser = ko.pureComputed(function () {
+        var token = authenticationToken();
+        var split = token.split("\.");
+        var userPayload = JSON.parse(jwt.base64urldecode(split[1]));
+        return userPayload.userName;
+    });
+
+    var logout = function () {
+        server.logout(authenticationToken());
+        sessionStorage.clear();
+        document.location.reload(true);
+    };
     //init function
     var init = function () {
+        //initialize errors
+        credential.errors = ko.validation.group(credential);
         var token = sessionStorage.getItem("token");
         if (token == null) {
             authenticationToken(false);
@@ -53,8 +82,11 @@ var Authenticator = function (serverModule) {
     return {
         isAuthenticated: isAuthenticated,
         credential: credential,
+        showAuthenticationFailed: showAuthenticationFailed,
+        loggedInUser: loggedInUser,
         getAuthenticationToken: getAuthenticationToken,
         setCallBack: setCallBack,
-        login: login
+        login: login,
+        logout: logout
     };
 };
